016a55eda6bc8eb095df1d932bba4f6c5f7a638b
mohdt-optimus <mohd.taha@optimusinfo.com>
2015-08-04 08:35:23 +0530

    Implemented the change suggested. Commenetd the code. Added keyboard dismiss feature on tapping anywhere else in view

--- photo_edit/ViewController.m
+++ photo_edit/ViewController.m
@@ -16,7 +16,7 @@
 -(void)shrinkInOutText:(UIPinchGestureRecognizer *)pinchGestureRecognizer1;
 -(void)rotateImage:(UIRotationGestureRecognizer *)rotationGestureRecognizer;
 -(void)rotateText:(UIRotationGestureRecognizer *)rotationGestureRecognizer1;
-
+    //There are the methods which will be used for gesture recognition.
 
 
 
@@ -39,23 +39,23 @@
     
     UIPanGestureRecognizer *moveImageGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveImage:)];
     [self.mainView addGestureRecognizer:moveImageGesture];
-    
+    //This is for movement of image in the view with gesture.
     UIPanGestureRecognizer *moveTextGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveText:)];
     [self.textView addGestureRecognizer:moveTextGesture];
-    
+      //This is for movement of textField in the view with gesture.
 
     UIPinchGestureRecognizer *shrinkImageGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(shrinkInOutImage:)];
     [self.mainView addGestureRecognizer:shrinkImageGesture];
-    
+      //This is for zooming and shrinking of image in the view with gesture.
     UIPinchGestureRecognizer *shrinkTextGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(shrinkInOutText:)];
     [self.textView addGestureRecognizer:shrinkTextGesture];
-    
+      //This is for zooming and shrinking of textField in the view with gesture.
     UIRotationGestureRecognizer *rotateImageGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotateImage:)];
     [self.mainView addGestureRecognizer:rotateImageGesture];
-    
+     //This is for rotating of image in the view with gesture.
     UIRotationGestureRecognizer *rotateTextGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(rotateText:)];
     [self.textView addGestureRecognizer:rotateTextGesture];
-
+    //This is for rotating of text field in the view with gesture.
     
     
     
@@ -76,7 +76,7 @@
 }
 
 - (IBAction)camera:(id)sender
-{
+{           //To select image from camera, this will open up the camera to take picture
     
     UIImagePickerController *photoPicker = [[UIImagePickerController alloc] init];
     photoPicker.delegate = self;
@@ -86,7 +86,7 @@
 }
 
 - (IBAction)gallery:(id)sender
-{
+{       //To select image from gallery, this will open up the gallery to select picture
     UIImagePickerController *photoPicker = [[UIImagePickerController alloc] init];
     photoPicker.delegate = self;
     photoPicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
@@ -100,7 +100,7 @@
 }
 
 - (IBAction)save:(id)sender
-{
+{               //This button action will save the image as it is in the view (Edited image)
     UIGraphicsBeginImageContext(_viewScreen.frame.size);
     [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
@@ -119,7 +119,7 @@
     return image;
 }
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
-{
+{                           //If image is saved successfully then Success message will be shown otherwise unsuccess message will be displayed
     NSString *alertTitle;
     NSString *alertMessage;
     
@@ -144,26 +144,26 @@
 
 
 -(void)moveImage:(UIPanGestureRecognizer *)panGestureRecognizer
-{
+{                          //This is for recognising and performing move gesture on image
     CGPoint touchLocation = [panGestureRecognizer locationInView:self.view];
     self.mainView.center = touchLocation;
 }
 
 -(void)moveText:(UIPanGestureRecognizer *)panGestureRecognizer
-{
+{                           //This is for recognising and performing move gesture on text
     CGPoint touchLocation = [panGestureRecognizer locationInView:self.view];
     self.textView.center = touchLocation;
 }
 
 
 -(void)shrinkInOutImage:(UIPinchGestureRecognizer *)pinchGestureRecognizer
-{
+{                          //This is for recognising and performing shrink in and out gesture on image
     self.mainView.transform = CGAffineTransformScale(self.mainView.transform, pinchGestureRecognizer.scale, pinchGestureRecognizer.scale);
     
     pinchGestureRecognizer.scale = 1.0;
 }
 -(void)shrinkInOutText:(UIPinchGestureRecognizer *)pinchGestureRecognizer
-{
+{                   //This is for recognising and performing shrink in and out gesture on text
     self.textView.transform = CGAffineTransformScale(self.textView.transform, pinchGestureRecognizer.scale, pinchGestureRecognizer.scale);
     
     pinchGestureRecognizer.scale = 1.0;
@@ -171,19 +171,22 @@
 
 
 -(void)rotateImage:(UIRotationGestureRecognizer *)rotationGestureRecognizer
-{
+{                   //This is for recognising and performing rotateion gesture on image
     self.mainView.transform = CGAffineTransformRotate(self.mainView.transform, rotationGestureRecognizer.rotation);
     
     rotationGestureRecognizer.rotation = 0.0;
 }
 
 -(void)rotateText:(UIRotationGestureRecognizer *)rotationGestureRecognizer
-{
+{               //This is for recognising and performing rotation gesture on image
     self.textView.transform = CGAffineTransformRotate(self.textView.transform, rotationGestureRecognizer.rotation);
     
     rotationGestureRecognizer.rotation = 0.0;
 }
-
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{               //This will dismiss keyboard when user touches any where in the view
+    [self.view endEditing:YES];
+}
 
 
 @end
